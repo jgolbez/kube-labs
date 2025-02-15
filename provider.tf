674c1b5305500026b35d5649a54e2630b546b3cd
Tim McC <jgolbez@gmail.com>
2025-02-15 17:05:10 +0000

    fixed provider.tf

--- provider.tf
+++ provider.tf
@@ -21,29 +21,19 @@ provider "aws" {
   # AWS_SECRET_ACCESS_KEY
   # AWS_DEFAULT_REGION
 }
-# This is our default provider configuration, which will be used for any
-# kubernetes resources that don't explicitly specify a provider
+
+# Add these new data source blocks after your existing configuration
+data "aws_eks_cluster_auth" "cluster" {
+  name = "my-eks-cluster"
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = "my-eks-cluster"
+}
+
+# Add the Kubernetes provider configuration after the data sources
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args = [
-      "eks",
-      "get-token",
-      "--cluster-name",
-      data.aws_eks_cluster.cluster.name
-    ]
-  }
-}
-
-# Add these data sources at the end of your provider.tf file
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
-  
-  depends_on = [
-    module.eks
-  ]
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
